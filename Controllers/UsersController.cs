f16a01e3ed8b121ad510bb1c782468ecc4916645
thomsolo <thomsolo@kean.edu>
2023-02-17 22:41:53 -0500

    Add project files.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ThomsoloMVCApp.Models;
+
+namespace ThomsoloMVCApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        UsersContext uc = new();
+        public IActionResult YKView()
+        {
+            return View(uc.YkumarTables.ToList());
+        }
+    }
+}
---
+++ Models/YkumarTable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThomsoloMVCApp.Models;
+
+public partial class YkumarTable
+{
+    public int UserId { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? UserEmail { get; set; }
+}

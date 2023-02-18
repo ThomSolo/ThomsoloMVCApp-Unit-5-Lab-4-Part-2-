224bd91d134919c8655e5b10bf425c3536223f7a
thomsolo <thomsolo@kean.edu>
2023-02-17 22:41:53 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ThomsoloMVCApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
